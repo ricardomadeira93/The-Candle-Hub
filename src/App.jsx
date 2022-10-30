e1a6b0ff9bd93c7ddbbaeb1942eb4b65a44824f5
Ricardo Madeira <ricardomadeira1993@gmail.com>
2022-10-30 21:30:34 +0000

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./assets/components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+    return(
+        <div className="Navbar">
+           <h1>THE CANDLE HUB</h1>
+        </div>
+    )
+}
